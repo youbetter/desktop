4a8f1d8344c028ef979f6f8de9e2d807a8a02b69
Sean Jacke <sean.jacke@learnship.com>
2016-01-15 13:29:23 +0100

    removes redux from stack

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute } from 'react-router';
 import { createHistory } from 'history';
-import { syncReduxAndRouter, routeReducer as routing } from 'redux-simple-router';
-import PouchMiddleware from 'pouch-redux-middleware';
 import PouchDB from 'pouchdb';
 
 // To use PouchDB-Fauxton Chrome Extension, window.PouchDB needs to be set
@@ -20,47 +16,15 @@ import ExerciseEditor from './components/ExerciseEditor.jsx';
 import './styles/bootstrap.css';
 
 const db = new PouchDB('you-better');
-const reducer = combineReducers({ routing });
 const history = createHistory();
-const store = applyMiddleware(
-    PouchMiddleware([
-        {
-            path: '/journal/entries',
-            db,
-            actions: {
-              insert: doc => store.dispatch({type: 'INSERT_ENTRY', entry: doc}),
-              update: doc => store.dispatch({type: 'UPDATE_ENTRY', entry: doc})
-            }
-        },
-        {
-            path: '/workout/exercises',
-            db,
-            actions: {
-                insert: doc => store.dispatch({type: 'INSERT_EXERCISE', exercise: doc}),
-                update: doc => store.dispatch({type: 'UPDATE_EXERCISE', exercise: doc}),
-                remove: doc => store.dispatch({type: 'DELETE_EXERCISE', exercise: doc})
-            }
-        }
-    ])
-)(createStore)(reducer);
-
-console.log('type of db:', typeof db);
-
-store.subscribe(() => {
-    console.log('store state:', store.getState());
-});
-
-syncReduxAndRouter(history, store);
 
 render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Route path="/" component={App}>
-                <IndexRoute component={WelcomeMessage} />
-                <Route path="journal" component={Journal} />
-                <Route path="exercise-editor" component={ExerciseEditor} />
-            </Route>
-        </Router>
-    </Provider>,
+    <Router history={history}>
+        <Route path="/" component={App}>
+            <IndexRoute component={WelcomeMessage} />
+            <Route path="journal" component={Journal} />
+            <Route path="exercise-editor" component={ExerciseEditor} />
+        </Route>
+    </Router>,
     document.getElementById('root')
 );
