fe9f8062413ef4ddb521b7a6861e2f3e788b0fab
Sean Jacke <sean.jacke@learnship.com>
2016-01-18 12:08:11 +0100

    removes router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import WelcomeMessage from './WelcomeMessage.jsx';
+import Journal from './Journal.jsx';
+import ExerciseEditor from './ExerciseEditor.jsx';
 
-export default class App extends Component {
-    render () {
+const NavbarLink = React.createClass({
+    render: function () {
+        return (
+            <li className="{this.props.isActive ? 'active' : ''}">
+                <a href="#" onClick={this.props.clickHandler}>{this.props.children}</a>
+            </li>
+        );
+    }
+});
+
+//TODO Include JS for Bootstrap Navbar
+const App = React.createClass({
+    getInitialState: function () {
+        return { activeLink: 'welcome' };
+    },
+    render: function () {
+        const app = this;
+        const links = [
+            { id: 'journal', name: 'Journal' },
+            { id: 'exercises', name: 'Exercises' }
+        ];
+        var activeChild;
+
+        function generateOnClick (linkId) {
+            return function (e) {
+                e.preventDefault();
+
+                app.setState({ activeLink: linkId });
+            };
+        }
+
+        switch (this.state.activeLink) {
+            case 'journal':
+                activeChild = <Journal></Journal>;
+                break;
+            case 'exercises':
+                activeChild = <ExerciseEditor></ExerciseEditor>;
+                break;
+            case 'welcome':
+            default:
+                activeChild = <WelcomeMessage></WelcomeMessage>;
+        }
+       
         return (
             <div id="app">
                 <nav className="navbar navbar-default">
@@ -19,18 +62,31 @@ export default class App extends Component {
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span> </button>
-                            <Link to="/" className="navbar-brand"><i>YOU BETTER</i></Link>
+                            <a href="#" onClick={generateOnClick('welcome')} className="navbar-brand">
+                                <i>YOU BETTER</i>
+                            </a>
                         </div>
                         <div className="collapse navbar-collapse" id="navbar-links">
                             <ul className="nav navbar-nav">
-                                <li><Link to="/journal">Journal</Link></li>
-                                <li><Link to="/exercise-editor">Exercises</Link></li>
+                                {
+                                    links.map(function (link) {
+                                        return (
+                                            <NavbarLink
+                                                key={link.id}
+                                                clickHandler={generateOnClick(link.id)}
+                                                isActive={app.state.activeLink === link.id}
+                                            >{link.name}</NavbarLink>
+                                        );
+                                    })
+                                }
                             </ul>
                         </div>
                     </div>
                 </nav>
-                {this.props.children}
+                {activeChild}
             </div>
         );
     }
-}
+});
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute } from 'react-router';
-import { createHistory } from 'history';
 import PouchDB from 'pouchdb';
 
 // To use PouchDB-Fauxton Chrome Extension, window.PouchDB needs to be set
@@ -9,22 +7,7 @@ import PouchDB from 'pouchdb';
 window.PouchDB = PouchDB;
 
 import App from './components/App.jsx';
-import WelcomeMessage from './components/WelcomeMessage.jsx';
-import Journal from './components/Journal.jsx';
-import ExerciseEditor from './components/ExerciseEditor.jsx';
 
 import './styles/bootstrap.css';
 
-const db = new PouchDB('you-better');
-const history = createHistory();
-
-render(
-    <Router history={history}>
-        <Route path="/" component={App}>
-            <IndexRoute component={WelcomeMessage} />
-            <Route path="journal" component={Journal} />
-            <Route path="exercise-editor" component={ExerciseEditor} />
-        </Route>
-    </Router>,
-    document.getElementById('root')
-);
+render(<App></App>, document.getElementById('root'));
